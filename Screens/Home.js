c786b17da1dd9cec2f6167f7da9345e0a7f856b7
Suneel16699 <143600321+Suneel16699@users.noreply.github.com>
2023-09-15 15:33:25 +0530

    Update Home.js

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,14 +1,16 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { NavigationContainer } from "@react-navigation/native";
-import ChatScreen from "./ChatScreen";
-
-const Stack = createNativeStackNavigator();
 import {
   Text,
-  View,
-  ScrollView,
+  Image,
   Pressable,
+  ScrollView,
+  View,
+  TextInput,
+  FlatList
 } from "react-native";
+import axios from 'axios';
+
+import { useState, useContext, createContext, useEffect } from "react";
+import { mockMessageData } from "./Home";
 
 function useMyApi(url){
   const [data, setData] = useState('');
@@ -25,80 +27,192 @@ function useMyApi(url){
 
     return data;
   }
+const MessagingContext = createContext();
 
-function ChatRow(props) {
-  const nam = props.items.name.split(" ");
+export default function ChatScreen({ route }) {
+  const messagingData = useMyApi('https://mocki.io/v1/b2b0486d-76d4-4377-a70d-7e4193949666');
+  const [input, setInput] = useState("");
+  const [countID, setCountID] = useState(messagingData?.length);
+  const [messageData, setMessagingData] = useState(messagingData);
+  const user = route.params.paramKey;
+  const nam = user.name.split(" ");
+  const navigation = route.params.navigation;
+
+  useEffect(() => {
+    setMessagingData({ id: countID, first: input, second: input });
+    setInput("");
+  }, [countID]);
+
   return (
-    <Pressable onPress=
-    {
-      ()=>{
-        props.nav.navigate('ChatScreen',{paramKey:props.items, navigation:props.nav});
-        props.nav.setOptions({headerShown:false});
-      }
-    } style={{borderBottomWidth: 0.3,
-      borderBottomColor :"#ccc",
-      backgroundColor: props.items.id == 1 ? "#dfdfdf" : "transparent",}}>
+    <MessagingContext.Provider value={messageData}>
+      <View style={{flexDirection:'column'}}>
       <View
         style={{
+          borderBottomWidth: 0.3,
+          borderBottomColor: "#ccc",
           flexDirection: "row",
-          left: 10,
-          height: 70,
-          alignItems: "center",
+          height:'11%'
         }}
       >
-        <View style={{width:'10%'}}>
-          <Text
+        <Pressable
+          onPress={() => {
+            navigation.pop();
+          }}
+        >
+          <Image
+            style={{ width: 24, height: 30, margin: 20 }}
+            source={{
+              uri: "https://cdn3.iconfinder.com/data/icons/google-material-design-icons/48/ic_arrow_back_48px-128.png",
+            }}
+          />
+        </Pressable>
+        <View
+          style={{
+            flexDirection: "row",
+            left: 10,
+            height: 70,
+            alignItems: "center",
+          }}
+        >
+          <View>
+            <Text onPress={()=>{alert("Oops , Not able to get the details :(")}}
+              style={{
+                textAlignVertical: "center",
+                textAlign: "center",
+                color: "white",
+                justifyContent: "center",
+                borderRadius: 50,
+                width: 52,
+                height: 52,
+                fontSize: 20,
+                backgroundColor: user.iconbg,
+              }}
+            >
+              {nam[0][0]}
+              {nam[1][0]}
+            </Text>
+          </View>
+          <View 
             style={{
-              textAlignVertical: "center",
-              textAlign: "center",
-              color: "white",
+              left: 10,
+              width: 160,
+              alignContent: "center",
               justifyContent: "center",
-              borderRadius: 50,
-              width: 52,
-              height: 52,
-              fontSize:20,
-              backgroundColor: props.items.iconbg,
             }}
           >
-            {nam[0][0]}
-            {nam[1][0]}
-          </Text>
+            <Text onPress={()=>{alert("Oops , Not able to get details :(")}}
+             style={{ fontWeight: "bold", fontSize: 16 }}>
+              {user.name}
+            </Text>
+            <Text onPress={()=>{alert("Oops , Not able to get details :(")}} ellipsizeMode="tail" numberOfLines={1}>
+              Online
+            </Text>
+          </View>
         </View>
-        <View
-          style={{ left: 30, width :'70%', alignContent: "center", justifyContent: "center" }}
-        >
-          <Text style={{ fontWeight: "bold", fontSize: 16 }}>
-            {props.items.name}
-          </Text>
-          <Text ellipsizeMode="tail" numberOfLines={1}>{props.items.message}</Text>
-        </View>
-        <View style={{ width:'10%' ,left:10, justifyContent:"center" , alignContent:"center"}}>
-          <Text>{props.items.time}</Text>
-          <Text style={{ textAlign:"center", marginLeft:props.items.count<10?7:0,marginRight:props.items.count<10?7:0 ,color:"#fff", borderRadius:50 , backgroundColor:"#54b059"}}>{props.items.count}</Text>
-        </View>
+        <Pressable onPress={()=>{
+          alert("Calling is not possible now :(")
+        }}>
+          <Image
+            style={{ width: 25, height: 25, marginVertical: 24 }}
+            source={{
+              uri: "https://cdn0.iconfinder.com/data/icons/google-material-design-3-0/48/ic_call_48px-128.png",
+            }}
+          />
+        </Pressable>
+        <Pressable onPress={()=>{alert("Options are not added yet :(")}}>
+          <Image
+            style={{
+              width: 25,
+              height: 25,
+              marginLeft: 24,
+              marginVertical: 24,
+            }}
+            source={{
+              uri: "https://cdn3.iconfinder.com/data/icons/user-interface-1-10/24/icon-ui-1-options-128.png",
+            }}
+          />
+        </Pressable>
       </View>
-    </Pressable>
+      <View style={{height:'82%'}}>
+        <ChatMessages />
+      </View>
+      <View style={{height:'6%' ,top:5,flexDirection: "row"}}>
+        <TextInput
+          value={input}
+          onChangeText={setInput}
+          inputMode="text"
+          placeholder="text your message"
+          style={{
+            borderWidth: 1,
+            width: '84%',
+            height:'100%',
+            fontSize: 20,
+            borderRadius: 10,
+            left:10,
+            paddingLeft:6
+          }}
+        ></TextInput>
+        <Pressable style={{width:'10%' ,height:'100%', left:10}}
+          onPress={() => {
+            setCountID(countID + 1);
+          }}
+        >
+          <Image
+            style={{ width:'auto', height:'70%', margin: 6 }}
+            source={{
+              uri: "https://cdn4.iconfinder.com/data/icons/glyphs/24/icons_send-128.png",
+            }}
+          ></Image>
+        </Pressable>
+      </View>
+      </View>
+    </MessagingContext.Provider>
   );
 }
 
-function ChatList({navigation}){
-  const data = useMyApi('https://mocki.io/v1/4f3f8561-0a4d-4a5f-90bb-25e460ba03f5');
-  return(
-    <View>
-    <ScrollView>
-    {data.map((val, index) => {
-      return <ChatRow key = {index} items={val} nav={navigation} />;
-    })}
-  </ScrollView>
-  </View>
-  );
-}
-
-export default function Home() {
+function ChatMessages() {
+  const messageData = useContext(MessagingContext);
+  if (messageData?.first !== "" && messageData?.id >= messagingData?.length + 1) {
+    messagingData.push(messageData);
+  }
   return (
-   <Stack.Navigator initialRouteName="ChatList">
-    <Stack.Screen name="ChatList" component={ChatList} options={{headerShown:false}}/>
-    <Stack.Screen name="ChatScreen" component={ChatScreen} options={{headerShown:false}}/>
-   </Stack.Navigator>
+    <FlatList style={{ backgroundColor: "#345" }}
+      data={messagingData}
+      renderItem = {(item)=><Message items={item}/>}
+      keyExtractor={item => item.id}
+    />
   );
 }
+
+const Message = (props) => {
+  return (
+    <View style={{flex:1}}>
+      <Text
+        style={{
+          borderWidth: 1,
+          borderRadius: 20,
+          padding: 5,
+          backgroundColor: "#ede",
+          width: 220,
+          margin: 2,
+          flex:1
+        }}
+      >
+        {props.items.item.first}
+      </Text>
+      <Text
+        style={{
+          marginLeft: 140,
+          borderWidth: 1,
+          borderRadius: 20,
+          padding: 5,
+          backgroundColor: "#ded",
+          width: 218,
+          marginVertical: 2,
+        }}
+      >
+        {props.items.item.second}
+      </Text>
+    </View>
+  );
+};
