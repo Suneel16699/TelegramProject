852a7ee2d0b7265484b1da5e87aeb7d3bb5518dc
Suneel16699 <143600321+Suneel16699@users.noreply.github.com>
2023-09-15 15:32:23 +0530

    Update Home.js

--- Screens/Home.js
+++ Screens/Home.js
@@ -82,7 +82,7 @@ function ChatRow(props) {
 }
 
 function ChatList({navigation}){
-  const data = useMyApi('https://mocki.io/v1/b2b0486d-76d4-4377-a70d-7e4193949666');
+  const data = useMyApi('https://mocki.io/v1/4f3f8561-0a4d-4a5f-90bb-25e460ba03f5');
   return(
     <View>
     <ScrollView>
